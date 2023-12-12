f1879617d154ca9b38bb20904a953f435b839995
yayoimizuha <poko.peko.tomo@icloud.com>
2023-12-13 00:33:36 +0900

    Initial commit

---
+++ src/routes/Inference.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import {InferenceSession} from "onnxruntime-web";
+
+    const InferenceOption = {executionProviders: ['wasm']};
+    // const InferenceOption = {executionProviders: ['wasm', 'webgl']};
+    let session = InferenceSession.create("/face_recognition_sim.onnx", InferenceOption);
+</script>
+<div class="inference">
+
+</div>
